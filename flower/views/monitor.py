b7dd9349e3d8c9c60dfcf2067e4b6cd76a227e24
minq <mingyu.park@wantedlab.com>
2025-09-05 15:28:56 +0900

    feat: 메트릭에서 오프라인 워커인 경우를 삭제할 수 있도록

--- flower/views/monitor.py
+++ flower/views/monitor.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import
 
+import logging
 from collections import defaultdict
 
 from tornado import web
@@ -10,6 +11,9 @@ import prometheus_client
 from ..views import BaseHandler
 from ..utils.broker import Broker
 from ..api.control import ControlHandler
+
+
+logger = logging.getLogger(__name__)
 
 
 class Monitor(BaseHandler):
@@ -122,3 +126,29 @@ class Metrics(BaseHandler):
     @gen.coroutine
     def get(self):
         self.write(prometheus_client.generate_latest())
+
+    @web.authenticated
+    @gen.coroutine
+    def delete(self):
+        offline_workers = []
+        for worker_name, worker in self.application.events.state.workers.items():
+            if not worker.alive:
+                offline_workers.append(worker_name)
+        logger.info("offline workers: %s, %d", offline_workers, len(offline_workers))
+        for worker_name in offline_workers:
+            for collector in prometheus_client.REGISTRY._collector_to_names.keys():
+                if hasattr(collector, '_metrics') and hasattr(collector._metrics, 'keys'):
+                    for metric_key in list(collector._metrics.keys()):
+                        if isinstance(metric_key, tuple):
+                            if worker_name in metric_key:
+                                try:
+                                    del collector._metrics[metric_key]
+                                except KeyError:
+                                    pass
+                        elif metric_key == worker_name:
+                            try:
+                                del collector._metrics[metric_key]
+                            except KeyError:
+                                pass
+        self.write("OK")
+        self.set_header("Content-Type", "text/plain")
